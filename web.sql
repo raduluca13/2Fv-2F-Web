9391ea1fede2d6a31c0ba699ccb58661d2b296da
Iftimesei Ioan <iftimesei.ioan@yahoo.com>
2018-06-12 00:02:19 +0300

    real project-third blood

--- web.sql
+++ web.sql
@@ -1,92 +1,86 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Jun 05, 2018 at 04:32 PM
--- Server version: 5.7.19
--- PHP Version: 7.1.9
+/*
+Navicat MySQL Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+Source Server         : ProiectTW
+Source Server Version : 80011
+Source Host           : localhost:3306
+Source Database       : tw
 
+Target Server Type    : MYSQL
+Target Server Version : 80011
+File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+Date: 2018-06-12 00:00:57
+*/
 
---
--- Database: `web`
---
+SET FOREIGN_KEY_CHECKS=0;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `categorii_punctaje`
---
-
+-- ----------------------------
+-- Table structure for categorii_punctaje
+-- ----------------------------
 DROP TABLE IF EXISTS `categorii_punctaje`;
-CREATE TABLE IF NOT EXISTS `categorii_punctaje` (
+CREATE TABLE `categorii_punctaje` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nume` text NOT NULL,
   `punctaj` int(2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of categorii_punctaje
+-- ----------------------------
 
---
--- Table structure for table `external`
---
-
+-- ----------------------------
+-- Table structure for external
+-- ----------------------------
 DROP TABLE IF EXISTS `external`;
-CREATE TABLE IF NOT EXISTS `external` (
+CREATE TABLE `external` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `src` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of external
+-- ----------------------------
 
---
--- Table structure for table `note`
---
-
+-- ----------------------------
+-- Table structure for note
+-- ----------------------------
 DROP TABLE IF EXISTS `note`;
-CREATE TABLE IF NOT EXISTS `note` (
+CREATE TABLE `note` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nr_matricol` varchar(13) NOT NULL,
   `categorie` int(11) NOT NULL,
   `valoare` int(11) NOT NULL,
   `data_notare` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of note
+-- ----------------------------
 
---
--- Table structure for table `plan_grupe`
---
-
+-- ----------------------------
+-- Table structure for plan_grupe
+-- ----------------------------
 DROP TABLE IF EXISTS `plan_grupe`;
-CREATE TABLE IF NOT EXISTS `plan_grupe` (
+CREATE TABLE `plan_grupe` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `grupa` varchar(2) NOT NULL,
   `id_prof` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of plan_grupe
+-- ----------------------------
 
---
--- Table structure for table `prezente`
---
-
+-- ----------------------------
+-- Table structure for prezente
+-- ----------------------------
 DROP TABLE IF EXISTS `prezente`;
-CREATE TABLE IF NOT EXISTS `prezente` (
+CREATE TABLE `prezente` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nr_matricol` varchar(13) NOT NULL,
   `categorie` int(11) NOT NULL,
@@ -94,14 +88,15 @@ CREATE TABLE IF NOT EXISTS `prezente` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of prezente
+-- ----------------------------
 
---
--- Table structure for table `profesori`
---
-
+-- ----------------------------
+-- Table structure for profesori
+-- ----------------------------
 DROP TABLE IF EXISTS `profesori`;
-CREATE TABLE IF NOT EXISTS `profesori` (
+CREATE TABLE `profesori` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nume` text NOT NULL,
   `prenume` text NOT NULL,
@@ -109,14 +104,15 @@ CREATE TABLE IF NOT EXISTS `profesori` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of profesori
+-- ----------------------------
 
---
--- Table structure for table `studenti`
---
-
+-- ----------------------------
+-- Table structure for studenti
+-- ----------------------------
 DROP TABLE IF EXISTS `studenti`;
-CREATE TABLE IF NOT EXISTS `studenti` (
+CREATE TABLE `studenti` (
   `id_student` int(11) NOT NULL AUTO_INCREMENT,
   `nr_matricol` varchar(13) NOT NULL,
   `nume` text NOT NULL,
@@ -128,35 +124,43 @@ CREATE TABLE IF NOT EXISTS `studenti` (
   UNIQUE KEY `nr_matricol` (`nr_matricol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of studenti
+-- ----------------------------
 
---
--- Table structure for table `sugestii`
---
-
+-- ----------------------------
+-- Table structure for sugestii
+-- ----------------------------
 DROP TABLE IF EXISTS `sugestii`;
-CREATE TABLE IF NOT EXISTS `sugestii` (
+CREATE TABLE `sugestii` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `categorie` enum('curs','laborator','others') NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of sugestii
+-- ----------------------------
 
---
--- Table structure for table `users`
---
-
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
-  `pass` varchar(255) DEFAULT NULL,
-  `user_type` enum('stud','prof','admin') DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `first_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `last_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `github_account` varchar(255) NOT NULL,
+  `facebook_account` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `user_type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-COMMIT;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('11', 'iftimesei.ioan@yahoo.com', 'a02df1fd80baa5565e9e1dc81c6c623b', 'Ioan', 'Iftimesei', 'hub.com/IftimeseiIoan', 'https://www.facebook.com/ioan.iftimesei', 'male', 'stud');
+INSERT INTO `users` VALUES ('13', 'iftimesei.cretu@yahoo.com', 'a3dcb4d229de6fde0db5686dee47145d', 'cretu', 'marius', 'asd', 'asd', 'male', 'stud');
